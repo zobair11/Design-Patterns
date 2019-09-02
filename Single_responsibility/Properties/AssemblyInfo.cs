cb20c80cb21659c7d49a75de47d0327a2fc6f561
Zobair Islam <zobair619316@hotmail.com>
2019-09-02 13:27:49 +0600

    single-responsibility

--- Single_responsibility/Properties/AssemblyInfo.cs
+++ Single_responsibility/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Single_responsibility")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Single_responsibility")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("43bfcb56-9d21-4342-8965-a03756be2db7")]
+[assembly: Guid("241f3f8b-a7ff-4013-bd4a-763accf2c43f")]
 
 // Version information for an assembly consists of the following four values:
 //
